e6908061eaf7d1c42398168820897d6577b7a0b1
kuhnkeF <35331054+kuhnkeF@users.noreply.github.com>
2022-06-08 14:32:37 +0200

    added HSVJitter to clip transforms
    defaults are used for training our models

--- clip_transforms.py
+++ clip_transforms.py
@@ -116,3 +116,44 @@ class RandomClipFlip:
                     clip[i] = cv2.flip(clip[i], 1)
 
         return clip
+
+class HSVJitter():
+
+    def __init__(self, h_range=10, s_range=25, v_range=20):
+        self.h_s_v_range = (h_range, s_range, v_range)
+
+    @staticmethod
+    def shift_hsv_uint8(img, hsv_shift):
+        # adaptated and modified from albumination (check albumentations.ai)
+        dtype = img.dtype
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        hue, sat, val = cv2.split(img)
+
+        lut_hue = np.arange(0, 256, dtype=np.int16)
+        lut_hue = np.mod(lut_hue + hsv_shift[0], 180).astype(dtype)
+
+        lut_sat = np.arange(0, 256, dtype=np.int16)
+        lut_sat = np.clip(lut_sat + hsv_shift[1], 0, 255).astype(dtype)
+
+        lut_val = np.arange(0, 256, dtype=np.int16)
+        lut_val = np.clip(lut_val + hsv_shift[2], 0, 255).astype(dtype)
+
+        hue = cv2.LUT(hue, lut_hue)
+        sat = cv2.LUT(sat, lut_sat)
+        val = cv2.LUT(val, lut_val)
+
+        img = cv2.merge((hue, sat, val)).astype(dtype)
+        img = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
+        return img
+
+    def __call__(self, clip):
+           # T W H C
+           # only works with uint8 images
+        hsv_shift = np.zeros((3,1), dtype=np.int16)
+        for i in range(3):
+            hsv_shift[i] = random.uniform(-self.h_s_v_range[i], self.h_s_v_range[i])
+
+        for i in range(clip.shape[0]):
+            clip[i, :, :, 0:3] = self.shift_hsv_uint8(np.squeeze(clip[i, :, :, 0:3]), hsv_shift)
+
+        return clip
